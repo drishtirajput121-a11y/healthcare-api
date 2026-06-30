5100a6e88b1d5eaa5f8ea46b8e5c492c62501b8e
drishtirajput121-a11y <drishtirajput121@gmail.com>
2026-06-30 22:54:52 +0530

    Add custom User model with email-based auth

--- core/settings.py
+++ core/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT'),
     }
 }
-
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
